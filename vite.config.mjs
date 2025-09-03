909d54ec7686498464c250c4b08abe2f743c26a1
Johnny Vardel <80030559+lemonadebusinesssolutions@users.noreply.github.com>
2025-09-03 12:35:35 +0100

    vite

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/body-perfect/' : '/',
   plugins: [react()],
   server: {
     port: 3000,
     open: true,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
   },
 });
