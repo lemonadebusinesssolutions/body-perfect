0f455b9c499294d027d95ca197c05c739ee0d98d
Johnny Vardel <80030559+lemonadebusinesssolutions@users.noreply.github.com>
2025-09-03 14:04:19 +0100

    2

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/body-perfect/' : '/',
@@ -12,5 +13,12 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        clinical_data: resolve(__dirname, 'public/clinical_data.html')
+      }
+    }
   },
+  publicDir: 'public'
 });
